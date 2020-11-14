69f24b84884ac8c50f993b87a1263a7008da16dd
sugarycoke <58714552+sugarycoke@users.noreply.github.com>
2020-11-14 14:36:06 -0500

    made it so we are good

--- censusstuff.py
+++ censusstuff.py
@@ -6,5 +6,11 @@ with open('population.json', 'r') as f:
 
 print(population)
 
+things = {}
+
 for item in population:
+    things.update({item['region'] + "_" + item['subregion']: {"population": item['population']}})
     
+
+with open("info.json", "w") as f:
+    f.write(json.dumps(things, indent=4))
